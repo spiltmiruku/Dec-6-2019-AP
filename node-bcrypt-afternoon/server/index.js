7bb0d54b03765fd6bf554a7b0a037e4a43513b36
spiltmiruku <spiltmiruku@gmail.com>
2019-12-08 17:11:00 -0700

    Fixed POST through testing in Postman

---
+++ node-bcrypt-afternoon/server/index.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const express = require("express"),
+      session = require("express-session"),
+      massive = require("massive"),
+      { CONNECTION_STRING, SESSION_SECRET } = process.env,
+      app = express(),
+      authCtrl = require("../server/controllers/authController");
+
+const PORT = 4000;
+
+app.use(express.json());
+
+massive(CONNECTION_STRING).then(db => {
+  app.set("db", db);
+  console.log("DB connected");
+});
+app.use(
+  session({
+    resave: true,
+    saveUninitialized: false,
+    secret: SESSION_SECRET
+    // cookie: {maxAge: 1000 * 60}
+  })
+);
+app.post("/auth/register", authCtrl.register);
+app.listen(PORT, () => console.log(`Server is ready on ${PORT}`));
